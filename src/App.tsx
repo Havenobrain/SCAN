4aec2291c0b4d4f2bac0affd8c60ca761a95f420
Havenobrain <107768179+Havenobrain@users.noreply.github.com>
2024-04-23 19:39:47 +0400

    11

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className={'h1'}>Hello world!</h1>
+    </div>
+  )
+}
+export default App
+
+
+function Button () {
+const Button = () => {
+  console.log('Hello!!!')
+  return <div>Button</div>;
+};
+
+  return (
+    <div className="App">
+      <button onClick={Button}>Запросить данные</button>
+    </div>
+  );
+  }
+
+  export default Button
