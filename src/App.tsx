da963583b7f28c12f583355da0f466de43eb7cdd
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-04-23 20:50:31 +0500

    Добавлены шрифты

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,22 @@
-import './App.css'
+import "./App.css";
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <h1 className={'h1'}>Hello world!</h1>
-    </div>
-  )
+export function App() {
+    return (
+        <div className="App">
+            <h1 className={"h1"}>Hello world!</h1>
+        </div>
+    );
 }
-export default App
 
+export function Button() {
+    const Button = () => {
+        console.log("Hello!!!");
+        return <div>Button</div>;
+    };
 
-function Button () {
-const Button = () => {
-  console.log('Hello!!!')
-  return <div>Button</div>;
-};
-
-  return (
-    <div className="App">
-      <button onClick={Button}>Запросить данные</button>
-    </div>
-  );
-  }
-
-  export default Button
+    return (
+        <div className="App">
+            <button onClick={Button}>Запросить данные</button>
+        </div>
+    );
+}
