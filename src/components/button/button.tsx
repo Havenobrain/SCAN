991f8f41f564e0bfb567039729ca3d3eb24bba90
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-04-23 21:40:59 +0500

    111

---
+++ src/components/button/button.tsx
@@ -0,0 +1,20 @@
+// const user: { name: string; id: number } = { id: 1, name: "" };
+// props - properties - свойства
+
+type ButtonProps = {
+    text: string;
+    number: number;
+    color: string;
+};
+
+export function Button(props: ButtonProps) {
+    function onClick() {
+        console.log(props.text);
+    }
+
+    return (
+        <button onClick={onClick} style={{ color: props.color }}>
+            Запросить данные {props.text} {props.number}
+        </button>
+    );
+}
